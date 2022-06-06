53379c779f72e67a3777cf7545f2b33e79d03a99
abidhossain2 <tanvirabid229@gmail.com>
2022-06-06 18:12:58 +0600

    home added

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+          <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
