3f53ac913651617db62ecedc527f17cf84331117
abidhossain2 <tanvirabid229@gmail.com>
2022-06-06 18:18:05 +0600

    protected route added

---
+++ src/Components/RequiredAuth/RequiredAuth.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loader from '../Loader/Loader';
+
+const RequiredAuth = ({ children }) => {
+    const [user, loading] = useAuthState(auth);
+    const location = useLocation();
+    if (loading) {
+        return <Loader></Loader>
+    }
+    if (!user) {
+        return <Navigate to="/signin" state={{ from: location }} replace />;
+    }
+    return children;
+};
+
+export default RequiredAuth;
