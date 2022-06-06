3e7d1f2a7c5457858c0d7c0a1c4c19839c0503c2
abidhossain2 <tanvirabid229@gmail.com>
2022-06-06 18:16:20 +0600

    not found page added

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.css'
+const NotFound = () => {
+    return (
+        <div className='no-result'>
+                <img src="https://img.freepik.com/free-vector/error-404-landing-page-with-road-sign-flat-style_249405-137.jpg?size=626&ext=jpg&ga=GA1.1.1180292963.1648229442" alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
